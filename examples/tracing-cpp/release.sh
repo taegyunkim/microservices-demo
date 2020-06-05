cc99a64d3d28843767242d9b0187b07421134b40
Taegyun Kim <k.taegyun@gmail.com>
2020-06-04 20:23:37 -0400

    comment out prints.

--- examples/tracing-cpp/release.sh
+++ examples/tracing-cpp/release.sh
@@ -26,26 +26,23 @@ namespace=$4
 app_labels=$(kubectl get deployments -L app | awk '{print $6}' | sed '1d')
 app_labels=($app_labels)
 
-for i in "${!app_labels[@]}"
-do
-    echo "$i=>${app_labels[i]}"
-done
+# for i in "${!app_labels[@]}"; do
+#     echo "$i=>${app_labels[i]}"
+# done
 
 version_labels=$(kubectl get deployments -L version | awk '{print $6}' | sed '1d')
 version_labels=($version_labels)
 
-for i in "${!version_labels[@]}"
-do
-    echo "$i=>${version_labels[i]}"
-done
+# for i in "${!version_labels[@]}"; do
+#     echo "$i=>${version_labels[i]}"
+# done
 
 if [ ${#app_labels[@]} != ${#version_labels[@]} ]; then
     echo "something went wrong, number of labels are different"
     exit 1
 fi
 
-for i in "${!app_labels[@]}"
-do
+for i in "${!app_labels[@]}"; do
     command="wasme deploy istio ${filter_name} \
     --id=${id}\
     --namespace=${namespace}\
@@ -53,11 +50,11 @@ do
     --config=\"{'name': '${app_labels[i]}-${version_labels[i]}'}\""
 
     echo $command
+    command
 done
 
 # version_label=$(kubectl get deployments -L version | awk '{print $6}' | sed '1d')
 # echo ${version_label}
-
 
 # for l in $app_labels; do
 #     echo "wasme deploy filter ${l} ${filter_name}"
