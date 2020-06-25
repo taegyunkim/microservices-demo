1e3461968b0ed8feb5fc363ebc6ba72b59dfd492
Taegyun Kim <k.taegyun@gmail.com>
2020-06-25 15:05:55 -0400

    Formatting.

--- src/recommendationservice/recommendation_server.py
+++ src/recommendationservice/recommendation_server.py
@@ -38,43 +38,53 @@ from grpc_health.v1 import health_pb2_grpc
 from logger import getJSONLogger
 logger = getJSONLogger('recommendationservice-server')
 
-def initStackdriverProfiling():
-  project_id = None
-  try:
-    project_id = os.environ["GCP_PROJECT_ID"]
-  except KeyError:
-    # Environment variable not set
-    pass
 
-  for retry in xrange(1,4):
+def initStackdriverProfiling():
+    project_id = None
     try:
-      if project_id:
-        googlecloudprofiler.start(service='recommendation_server', service_version='1.0.0', verbose=0, project_id=project_id)
-      else:
-        googlecloudprofiler.start(service='recommendation_server', service_version='1.0.0', verbose=0)
-      logger.info("Successfully started Stackdriver Profiler.")
-      return
-    except (BaseException) as exc:
-      logger.info("Unable to start Stackdriver Profiler Python agent. " + str(exc))
-      if (retry < 4):
-        logger.info("Sleeping %d seconds to retry Stackdriver Profiler agent initialization"%(retry*10))
-        time.sleep (1)
-      else:
-        logger.warning("Could not initialize Stackdriver Profiler after retrying, giving up")
-  return
+        project_id = os.environ["GCP_PROJECT_ID"]
+    except KeyError:
+        # Environment variable not set
+        pass
+
+    for retry in xrange(1, 4):
+        try:
+            if project_id:
+                googlecloudprofiler.start(
+                    service='recommendation_server', service_version='1.0.0', verbose=0, project_id=project_id)
+            else:
+                googlecloudprofiler.start(
+                    service='recommendation_server', service_version='1.0.0', verbose=0)
+            logger.info("Successfully started Stackdriver Profiler.")
+            return
+        except (BaseException) as exc:
+            logger.info(
+                "Unable to start Stackdriver Profiler Python agent. " + str(exc))
+            if (retry < 4):
+                logger.info(
+                    "Sleeping %d seconds to retry Stackdriver Profiler agent initialization" % (retry*10))
+                time.sleep(1)
+            else:
+                logger.warning(
+                    "Could not initialize Stackdriver Profiler after retrying, giving up")
+    return
+
 
 class RecommendationService(demo_pb2_grpc.RecommendationServiceServicer):
     def ListRecommendations(self, request, context):
         wasm_header = None
         for key, value in context.invocation_metadata():
             if key == "x-wasm-path":
-                logger.warn("x-wasm-path type: %s, value: %s", type(value), repr(value))
+                logger.warn("x-wasm-path type: %s, value: %s",
+                            type(value), repr(value))
                 wasm_header = value
                 break
-        metadata = (("x-wasm-path", wasm_header),) if wasm_header is not None else None
+        metadata = (("x-wasm-path", wasm_header),
+                    ) if wasm_header is not None else None
         max_responses = 5
         # fetch list of products from product catalog stub
-        cat_response = product_catalog_stub.ListProducts(demo_pb2.Empty(), metadata=metadata)
+        cat_response = product_catalog_stub.ListProducts(
+            demo_pb2.Empty(), metadata=metadata)
         product_ids = [x.id for x in cat_response.products]
         filtered_products = list(set(product_ids)-set(request.product_ids))
         num_products = len(filtered_products)
@@ -83,7 +93,8 @@ class RecommendationService(demo_pb2_grpc.RecommendationServiceServicer):
         indices = random.sample(range(num_products), num_return)
         # fetch product ids from indices
         prod_list = [filtered_products[i] for i in indices]
-        logger.info("[Recv ListRecommendations] product_ids={}".format(prod_list))
+        logger.info(
+            "[Recv ListRecommendations] product_ids={}".format(prod_list))
         # build and return response
         response = demo_pb2.ListRecommendationsResponse()
         response.product_ids.extend(prod_list)
@@ -93,61 +104,63 @@ class RecommendationService(demo_pb2_grpc.RecommendationServiceServicer):
         return health_pb2.HealthCheckResponse(
             status=health_pb2.HealthCheckResponse.SERVING)
 
+
 if __name__ == "__main__":
     logger.info("initializing recommendationservice")
 
     try:
-      if "DISABLE_PROFILER" in os.environ:
-        raise KeyError()
-      else:
-        logger.info("Profiler enabled.")
-        initStackdriverProfiling()
+        if "DISABLE_PROFILER" in os.environ:
+            raise KeyError()
+        else:
+            logger.info("Profiler enabled.")
+            initStackdriverProfiling()
     except KeyError:
         logger.info("Profiler disabled.")
 
     try:
-      if "DISABLE_TRACING" in os.environ:
-        raise KeyError()
-      else:
-        logger.info("Tracing enabled.")
-        sampler = always_on.AlwaysOnSampler()
-        exporter = stackdriver_exporter.StackdriverExporter(
-          project_id=os.environ.get('GCP_PROJECT_ID'),
-          transport=AsyncTransport)
-        tracer_interceptor = server_interceptor.OpenCensusServerInterceptor(sampler, exporter)
+        if "DISABLE_TRACING" in os.environ:
+            raise KeyError()
+        else:
+            logger.info("Tracing enabled.")
+            sampler = always_on.AlwaysOnSampler()
+            exporter = stackdriver_exporter.StackdriverExporter(
+                project_id=os.environ.get('GCP_PROJECT_ID'),
+                transport=AsyncTransport)
+            tracer_interceptor = server_interceptor.OpenCensusServerInterceptor(
+                sampler, exporter)
     except (KeyError, DefaultCredentialsError):
         logger.info("Tracing disabled.")
         tracer_interceptor = server_interceptor.OpenCensusServerInterceptor()
 
-
     try:
-      if "DISABLE_DEBUGGER" in os.environ:
-        raise KeyError()
-      else:
-        logger.info("Debugger enabled.")
-        try:
-          googleclouddebugger.enable(
-              module='recommendationserver',
-              version='1.0.0'
-          )
-        except Exception, err:
-            logger.error("Could not enable debugger")
-            logger.error(traceback.print_exc())
-            pass
+        if "DISABLE_DEBUGGER" in os.environ:
+            raise KeyError()
+        else:
+            logger.info("Debugger enabled.")
+            try:
+                googleclouddebugger.enable(
+                    module='recommendationserver',
+                    version='1.0.0'
+                )
+            except Exception, err:
+                logger.error("Could not enable debugger")
+                logger.error(traceback.print_exc())
+                pass
     except KeyError:
         logger.info("Debugger disabled.")
 
     port = os.environ.get('PORT', "8080")
     catalog_addr = os.environ.get('PRODUCT_CATALOG_SERVICE_ADDR', '')
     if catalog_addr == "":
-        raise Exception('PRODUCT_CATALOG_SERVICE_ADDR environment variable not set')
+        raise Exception(
+            'PRODUCT_CATALOG_SERVICE_ADDR environment variable not set')
     logger.info("product catalog address: " + catalog_addr)
     channel = grpc.insecure_channel(catalog_addr)
     product_catalog_stub = demo_pb2_grpc.ProductCatalogServiceStub(channel)
 
     # create gRPC server
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10),
-                      interceptors=(tracer_interceptor,))
+                         interceptors=(tracer_interceptor,))
 
     # add class to gRPC server
     service = RecommendationService()
@@ -161,7 +174,7 @@ if __name__ == "__main__":
 
     # keep alive
     try:
-         while True:
+        while True:
             time.sleep(10000)
     except KeyboardInterrupt:
-            server.stop(0)
+        server.stop(0)
